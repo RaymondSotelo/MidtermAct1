a4e558f30254a4cf82fc85e9562dab87a81b418d
RaymondSotelo <raymondsotelo75@gmail.com>
2025-09-26 19:31:21 +0800

    Index.php

--- Midterm/sotelo/Index.php
+++ Midterm/sotelo/Index.php
@@ -4,30 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href ="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php echo "";?>
-    <br>
-    <br>
-    
-    <!--<form action = "http://localhost/sotelo/Homepage.html"> -->
-        <h2>Student Registration Forms <h2>
-
-
-     <form action = "display.php" method = "POST">
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname"><br>
-        <label for="idnum">ID Number:</label><br>
-        <input type="text" id ="idNum" name = "idNum"><br>
-        <label for="department">Department:</label><br>
-        <input type="text" department ="department" name = "department"><br><br>
-
-        <input type="submit" value="Submit">
-
-      </form>
-      
+    <div class="mainCon">
+        <div>
+            <h1>Student Registration Form</h1>
+        </div>
+        <div>
+            <form action="notif.php" method="POST">
+                <div>
+                    <label for="fname">First name:</label><br>
+                    <input type="text" id="fname" name="fname" required>
+                </div>
+                <br>
+                <div>
+                    <label for="lname">Last name:</label><br>
+                    <input type="text" id="lname" name="lname" required>
+                </div>
+                <br>
+                <div>
+                    <label for="idnum">ID number:</label><br>
+                    <input type="text" id="idNum" name="idNum" required>
+                </div>
+                <br>
+                <div>
+                    <label for="course">Course:</label><br>
+                    <input type="text" id="course" name="course" required>
+                </div>
+                <br><br>
+                <div>
+                    <input type="submit" value="Submit">
+                    <br>
+                    <br>
+                    <input type="button" value="Show Student Info" onclick="window.location.href='display.php'">
+                </div>
+              </form> 
+        </div>
+    </div>
 </body>
 </html>
