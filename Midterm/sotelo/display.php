93dc2f32273cd2331d0649cbfdc5b3a79d8e46c8
RaymondSotelo <raymondsotelo75@gmail.com>
2025-09-26 19:30:58 +0800

    display.php

--- Midterm/sotelo/display.php
+++ Midterm/sotelo/display.php
@@ -1,9 +1,37 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
 <body>
+<div class="displayCon">
+    <form action="index.php">
+        <div>
+            <?php
+            $file = "studentInfo.txt";
 
-Welcome <?php echo $_POST["fname"]; ?><br>
-Your family Name is: <?php echo $_POST["lname"]; ?><br>
-My ID Number is: <?php echo $_POST["idNum"]; ?><br>
-Department: <?php echo $_POST["department"]; ?>
+            if(filesize($file) > 0){
+                $myfile = fopen("studentInfo.txt", "r") or die("Unable to open file!");
+                $content = fread($myfile,filesize("studentInfo.txt"));
+                fclose($myfile);
+
+                echo nl2br(htmlspecialchars($content));   
+            }else{
+                echo "The list is empty. Register more students to populate the list!";
+            }
+            ?>
+        </div>
+        <br>
+        <div>
+             <input type="Submit" value="Return">
+             <br>
+             <br>
+             <input type="button" value="Clear" onclick="window.location.href='clearNotif.php'">
+        </div>  
+    </form>
+</div>
 </body>
 </html>
